0f21c2adcd1524fd1fa90c5c6d57a47f6a8d4ec4
onurkslr <onurkoseler97@gmail.com>
2020-05-26 20:10:49 +0300

    backend with hibernate

--- src/main/java/com/javacourse/project/hibernateAndJpa/restApi/CityController.java
+++ src/main/java/com/javacourse/project/hibernateAndJpa/restApi/CityController.java
@@ -3,6 +3,7 @@ package com.javacourse.project.hibernateAndJpa.restApi;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.javacourse.project.hibernateAndJpa.Entities.*;
 import com.javacourse.project.hibernateAndJpa.Business.ICityService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api")					
 
@@ -22,19 +24,22 @@ public class CityController {
 	public CityController(ICityService cityService) {
 		this.cityService = cityService;
 	}
+	@CrossOrigin(origins ="http://localhost:4200/")
 	@GetMapping("/cities")
 	public List<City> get(){
 		return cityService.GetAll();
 	}
-	
+	@CrossOrigin(origins ="http://localhost:4200/add")
 	@PostMapping("/add")
 	public void add(@RequestBody City city) {
 		cityService.add(city);
 	}
+	@CrossOrigin(origins ="http://localhost:4200/update")
 	@PostMapping("/update")
 	public void update(@RequestBody City city) {
 		cityService.update(city);
 	}
+	@CrossOrigin(origins ="http://localhost:4200/delete")
 	@PostMapping("/delete")
 	public void delete(@RequestBody City city) {
 		cityService.delete(city);
